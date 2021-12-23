02477238054c083212bda1872d3d7ea5a4c19d52
siddhant gore <siddhantgore111@gmail.com>
2021-12-23 23:57:43 +0530

    deleted

--- day1/inp/out.py
+++
@@ -1,8 +0,0 @@
-x=int(input())
-y=int(input())
-ans=x*y
-
-if not ans < 0:
-    print(ans)
-else:
-    print("NA")
--- day1/palindrome.py
+++
@@ -1,8 +0,0 @@
-def palindrome(text:str) -> int:
-    text=text.casefold()
-    if text[::-1]==text:
-        return True
-    else:
-        return False
-
-print(palindrome("Mom"))
